49e01d61db4c8037363d8b517de4f7caa8162faf
JR-ASC <jr.asc.cl@gmail.com>
2017-01-14 16:08:08 -0300

    Update login.php

--- login.php
+++ login.php
@@ -33,19 +33,26 @@
                mysqli_close($enlace); 
             }    
     } 
+    //verificamos si existe la variable session.
     if(!isset($_SESSION['idUsuario'])){
+        //si no existe se verifica si se presiono el boton en el formulario
         if(null!==(filter_input(INPUT_POST, 'button-connection'))){
-                list($idUsuario,$estado)=loginUser($user, $pass, conectarBD());  
+            //si se presiono se toma el array de la funcion loginUser y se obtienen las variables de cada casilla en la funcion list
+                list($idUsuario,$estado)=loginUser($user, $pass, conectarBD());
+            //finalmente si la variable idusuario no esta vacia se crean las variables de tipo session y se redirecciona al index.php
             if($idUsuario!==NULL){
                 $_SESSION['idUsuario']=$idUsuario;
                 $_SESSION['estado']=$estado;
                 header("location: index.php");
             }else{
+                //si no se encuentra el idUsuario en la BD la variable seria null entrando a este else
                 echo '<script type="Text/Javascript">
                     alert("Usuario o Password incorrecto, intente nuevamente");
                     </script>'; 
             }  
         }
     } else {
+            /*en la redireccion ya existiran las variables de sesion asi que pasara directamente a este punto
+            donde seremos redirigidos a la pagina main-menu.php*/
             header("Location: main-menu.php");
       }
