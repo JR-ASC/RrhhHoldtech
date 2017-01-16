5561b49c98bf65a5ec90e9174d0b69e8afcb25e7
JR-ASC <jr.asc.cl@gmail.com>
2017-01-15 22:28:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--
-index.php trabaja con login.php ubicado en php/login.php, para poder obsevar el diseño de la pagina comentar el codigo php
-incrustado en el que se encuentra el include.
--->
 <?php
   include 'php/login.php';
   ?>
@@ -11,27 +7,16 @@ incrustado en el que se encuentra el include.
         <!--Import Google Icon Font-->
         <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta charset="utf-8"/>
         <title>Login Holdtech</title>
-        <style>
-            body {
-                display: flex;
-                min-height: 100vh;
-                flex-direction: column;
-            }
-
-            main {
-                flex: 1 0 auto;
-            }
-        </style>
     </head>
     <body>
         <nav>
             <div class="nav-wrapper orange">
-                <img class="brand-logo center"src="imagenes/logo-small.png" alt="holdtech" onclick="window.location.href='index.php'">
+                <img class="brand-logo center" src="imagenes/logo-small.png" alt="holdtech" onclick="window.location.href='index.php'">
                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                 <ul id="nav-mobile" class="left hide-on-med-and-down">
                     <li><a href="http://www.holdtech.cl/">Holdtech</a></li>
@@ -42,68 +27,70 @@ incrustado en el que se encuentra el include.
                 </ul>
             </div>
         </nav>
-        <!--parallax container-->
-        <div id="index-banner" class="parallax-container">
-            <div class="section no-pad-bot">
-                <div class="container">
-                    <br><br>
-                    <img class="header" src="imagenes/logo.png" alt="Holdtech"/>  
+        <main>
+            <!--parallax container-->
+            <div id="index-banner" class="parallax-container">
+                <div class="section no-pad-bot">
+                    <div class="container">
+                        <br><br>
+                        <img class="header" src="imagenes/logo.png" alt="Holdtech"/>  
+                    </div>
                 </div>
+                <div class="parallax"><img src="imagenes/slide2.jpg"></div>
             </div>
-            <div class="parallax"><img src="imagenes/slide2.jpg"></div>
-        </div>
-        <!--   Login section   -->
-        <div class="container">
-            <div class="section">
-                <div class="row">
-                    <div class="col s12 m4">
-                        <div class="icon-block">
-                            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
-                            <h5 class="center">Login</h5>
-                            <form class="col s11" action="" method="post">
-                                <div class="input-field">
-                                    <i class="material-icons prefix">perm_identity</i>
-                                    <input placeholder="Rut sin puntos ni guion" id="user" type="text" name="nombre-usuario" pattern="^([0-9]+[k]{0,1}){8,9}" title="Ese no parece un Rut valido." class="validate" required/>
-                                    <label class="active" for="user">Usuario:</label>
+            <!--   Login section   -->
+            <div class="container">
+                <div class="section">
+                    <div class="row">
+                        <div class="col s12 m4">
+                            <div class="icon-block">
+                                <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+                                <h5 class="center">Login</h5>
+                                <form class="col s11" action="" method="post">
+                                    <div class="input-field">
+                                        <i class="material-icons prefix">perm_identity</i>
+                                        <input placeholder="Rut sin puntos ni guion" id="user" type="text" name="nombre-usuario" pattern="^([0-9]+[k]{0,1}){8,9}" title="Ese no parece un Rut valido." class="validate" required/>
+                                        <label class="active" for="user">Usuario:</label>
+                                    </div>
+                                    <div class="input-field">
+                                        <i class="material-icons prefix">vpn_key</i>
+                                        <input id="user" type="password" name="contrasenia" class="validate" required/>
+                                        <label for="user">password:</label>
+                                    </div>
+                                    <div class="input-field center">
+                                        <button class="btn waves-effect waves-light orange" type="submit" name="button-connection">Ingresar
+                                            <i class="material-icons right">send</i>
+                                        </button>
+                                    </div>
+                                </form>
+                                <div class="input-field right">
+                                    <br>
+                                    <a class="grey-text text-darken-1" href="#!">¿Olvido su password?</a>                                   
                                 </div>
-                                <div class="input-field">
-                                    <i class="material-icons prefix">vpn_key</i>
-                                    <input id="user" type="password" name="contrasenia" class="validate" required/>
-                                    <label for="user">password:</label>
-                                </div>
-                                <div class="input-field center">
-                                    <button class="btn waves-effect waves-light orange" type="submit" name="button-connection">Ingresar
-                                        <i class="material-icons right">send</i>
-                                    </button>
-                                </div>
-                            </form>
-                            <div class="input-field right">
-                                <br>
-                                <a class="grey-text text-darken-1" href="#!">¿Olvido su password?</a>                                   
+                            </div>
+                        </div>
+                        <div class="col s12 m4 l4">
+                            <div class="icon-block">
+                                <h2 class="center brown-text"><i class="material-icons">cloud</i></h2>
+                                <h5 class="center">Sus datos en todo lugar:</h5>
+                                <p class="light">Con la aplicación web de holdtech, usted podra tener acceso a sus datos en todo lugar, disfrute de la compatibilidad con diferentes dispositivos moviles o de escritorio Android, Windows o Macintosh, usted podra revisar en linea su horario laboral, pre-liquidaciones, liquidaciones de sueldo, notas de calidad y asistencia ademas de tener a su dispocición enlaces de utilidad para verificacación de cotizaciones y caja de compensación.</p>
+                            </div>
+                        </div>
+                        <div class="col s12 m4 l4">
+                            <div class="icon-block">
+                                <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+                                <h5 class="center">Facíl de Usar:</h5>
+                                <p class="light">Con un diseño, elgante, limpio y sencillo basado en material desing de Google &REG; disponemos su información en pantalla de modo que esta se vizualice de forma clara y comoda aprovechando los espacios y haciendo que la navegacion sea rapida, comoda e intuitiva.</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col s12 m4 l4">
-                        <div class="icon-block">
-                            <h2 class="center brown-text"><i class="material-icons">cloud</i></h2>
-                            <h5 class="center">Sus datos en todo lugar:</h5>
-                            <p class="light">Con la aplicación web de holdtech, usted podra tener acceso a sus datos en todo lugar, disfrute de la compatibilidad con diferentes dispositivos moviles o de escritorio Android, Windows o Macintosh, usted podra revisar en linea su horario laboral, pre-liquidaciones, liquidaciones de sueldo, notas de calidad y asistencia ademas de tener a su dispocición enlaces de utilidad para verificacación de cotizaciones y caja de compensación.</p>
-                        </div>
-                    </div>
-                    <div class="col s12 m4 l4">
-                        <div class="icon-block">
-                            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
-                            <h5 class="center">Facíl de Usar:</h5>
-                            <p class="light">Con un diseño, elgante, limpio y sencillo basado en material desing de Google &REG; disponemos su información en pantalla de modo que esta se vizualice de forma clara y comoda aprovechando los espacios y haciendo que la navegacion sea rapida, comoda e intuitiva.</p>
-                        </div>
-                    </div>
                 </div>
             </div>
-        </div>
+        </main>
         <footer class="page-footer orange">
             <div class="container">
                 <div class="row">
-                    <div class="col l6 s12">
+                    <div class="col l6 m6 s12">
                         <h5 class="white-text">Holdtech S.A.</h5>
                         <p class="grey-text text-lighten-4">Contacto:</p>
                         <p class="grey-text text-lighten-4"><i class="material-icons prefix">phone</i> (+56 2) 2429 7770.</p>
@@ -129,10 +116,9 @@ incrustado en el que se encuentra el include.
         </footer>
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-      <!-- scrip para iniciar el contenedor del fondo movil -->
+      <script type="text/javascript" src="js/materialize.js"></script>
       <script src="js/init.js"></script>
-      <!-- script para inicializar el boton colapsable en version mobile -->
       <script> $(".button-collapse").sideNav();</script>
     </body>
 </html>
+
